186a85d704ac1efcc4d8c53368933f5dc07c48b6
timothy battafarano <tim@deepsprings.edu>
2023-10-05 15:19:16 -0500

    Using classes with generics

---
+++ stack.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+
+class Stack<T> {
+  final List<T> _items = []; // example of design pattern called composition
+
+  void push(T item) => _items.add(item);
+
+  T pop() => _items.removeLast();
+}
+
+void main() {
+  final stack = Stack<int>();
+  stack.push(1);
+  stack.push(2);
+  print(stack.pop());
+  print(stack.pop());
+  final names = Stack<String>();
+  names.push('Andrea');
+  names.push('Alice');
+}
