712349dc07d724647302c0f242af82bed551a054
timothy battafarano <tim@deepsprings.edu>
2023-10-05 13:00:06 -0500

    covariant keyword and equality operator

--- point.dart
+++ point.dart
@@ -4,11 +4,26 @@ class Point {
   final int y;
   @override
   String toString() => 'Point($x, $y)';
-  
-}
+
+
+  @override
+  bool operator ==(covariant Point other){ 
+    // covariant Point (vs just Object other)
+    // tells Dart: I want to only override this operator
+    // when it is called with an argument of type Point.
+    // and it should be invalid with any other type.
+    // if (other is Point) {
+      // I think this promotes x and y to type Point
+      // you only need the if statement if you use just Oject other.
+      return x == other.x && y == other.y;
+    // }
+    // return false;
+  }
+}  
 
 void main () {
-  print(Point(1, 1));
-  const list = [Point(1, 3), Point(3, 4)];
-  print(list);
+  print(Point(0, 0) == Point(0, 0));
+  print(Point(0,0) == 'abc'); //This won't compile because it's only overriden
+  // for type Point, not type String. I think...
+
 }
