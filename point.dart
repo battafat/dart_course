7d850db9b89bdc26eebdeaeef82729b458ec0a7f
timothy battafarano <tim@deepsprings.edu>
2023-10-05 14:21:09 -0500

    Exercise: Implement + and * operators

--- point.dart
+++ point.dart
@@ -2,28 +2,33 @@ class Point {
   const Point(this.x, this.y);
   final int x;
   final int y;
+
   @override
   String toString() => 'Point($x, $y)';
 
 
   @override
-  bool operator ==(covariant Point other){ 
-    // covariant Point (vs just Object other)
-    // tells Dart: I want to only override this operator
-    // when it is called with an argument of type Point.
-    // and it should be invalid with any other type.
-    // if (other is Point) {
-      // I think this promotes x and y to type Point
-      // you only need the if statement if you use just Oject other.
-      return x == other.x && y == other.y;
-    // }
-    // return false;
+  bool operator ==(covariant Point other) { 
+    return x == other.x && y == other.y;
   }
+
+  @override // Dart says this isn't necessary. Runs without it.
+  // Presumably because we're defining the operand for type Point, not bool or other inherited type...?
+  Point operator +(Point other) { 
+    return Point(x + other.x, y + other.y);
+  }
+
+  @override// Dart says this isn't necessary. Runs without it.
+  // Presumably because we're defining the operand for type Point, not bool or other inherited type...?
+  Point operator *(int other) { 
+    return Point(x * other, y * other);
+  }
+
 }  
 
 void main () {
-  print(Point(0, 0) == Point(0, 0));
-  print(Point(0,0) == 'abc'); //This won't compile because it's only overriden
-  // for type Point, not type String. I think...
+
+  print(Point(1, 1) + Point(2, 0));
+  print(Point(2, 1) * 5); 
 
 }
