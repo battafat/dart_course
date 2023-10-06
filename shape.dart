c7939e5e3ddf1d2a0b3b0b29e0bf917bcf3d7aef
timothy battafarano <tim@deepsprings.edu>
2023-10-06 15:06:27 -0500

    Exercise: JSON Serialization

--- shape.dart
+++ shape.dart
@@ -1,50 +1,65 @@
 import 'dart:math';
 
 abstract class Shape {
-  double get area;
-  double get perimeter;
-  void printValues() {
-    print('area: $area, perimeter: $perimeter');
+  double get area; 
+  const Shape();
+
+  factory Shape.fromJson(Map<String, Object> json) {
+    final type = json['type'];
+    switch (type) {
+      case 'square':
+        final side = json['side'];
+        if (side is double){
+        return Square(side);
+        }
+        throw UnsupportedError('invalid or missing side property');
+      case 'circle':
+        final radius = json['radius'];
+        if (radius is double) {
+          return Circle(radius);
+        }
+        throw UnsupportedError('invalid or missing radius property');
+      default: 
+        throw UnimplementedError('shape $type not recognized');
+    }
   }
 }
 
 class Square extends Shape {
-  Square(this.side);
+  const Square(this.side);
   final double side;
 
   @override
   double get area => side * side;
-  
-  @override
-  double get perimeter => 4 * side;
-
-
 }
 
 class Circle extends Shape {
-  Circle(this.radius);
+  const Circle(this.radius);
   final double radius;
   
   @override
   double get area => pi * radius * radius;
-  
-  @override
-  double get perimeter => 2 * pi * radius;
-
-
-
 }
 
-// void printArea(Shape shape) {
-//   print(shape.area);
-// }
-
+void printArea(Shape shape) {
+  print(shape.area);
+}
 
 
 void main() {
-  final shapes = [
-    Square(3),
-    Circle(4),
+  final shapesJson = [
+    {
+      'type': 'square',
+      'side': 10.0,
+    },
+    {
+      'type': 'circle',
+      'radius': 5.0,
+    },
+    {
+      'type': 'triangle',
+    }
   ];
-  shapes.forEach((shape) => shape.printValues());
+  final shapes = shapesJson.map((shapeJson) => Shape.fromJson(shapeJson));
+  shapes.forEach(printArea);
 }
